a6c5303f9e7f6d4524c864f59ccd275387568879
CarloMercuri <cmercuri85@gmail.com>
2021-10-07 10:40:33 +0200

    Add project files.

---
+++ BlaAndCamping/LogicControl/DataProcessor.cs
@@ -0,0 +1,21 @@
+﻿using BlaAndCamping.DataControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlaAndCamping.LogicControl
+{
+    public static class DataProcessor
+    {
+        private static DatabaseInterface _dbInterface = new DatabaseInterface("local");
+
+        public static CampingSpotTypeInformation GetSpotTypeInformation(int spotType)
+        {
+            return _dbInterface.GetCampingSpotTypeInformation(spotType);
+        }
+
+    }
+
+
+}
